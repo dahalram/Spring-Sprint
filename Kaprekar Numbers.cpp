d8d9dd77a22adfbbf2153b747db0f138c270e5f9
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-03-16 00:37:46 -0400

    Update Kaprekar Numbers.cpp

--- Kaprekar Numbers.cpp
+++ Kaprekar Numbers.cpp
@@ -1,3 +1,10 @@
+//Definition of Kaprekar numbers from Wikipedia
+/*
+In mathematics, a Kaprekar number for a given base is a non-negative integer, 
+the representation of whose square in that base can be split into two parts that add up to the original number again. 
+For instance, 45 is a Kaprekar number, because 45² = 2025 and 20+25 = 45.
+*/
+
 #include <iostream>
 #include <cmath>
 
